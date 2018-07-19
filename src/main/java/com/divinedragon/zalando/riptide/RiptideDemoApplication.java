ec131e1ac7db0c98537bd296dba7bbb73a90b170
Deepak Shakya <deepak.shakya@zalando.de>
2018-07-19 11:39:46 +0200

    Intial commit

---
+++ src/main/java/com/divinedragon/zalando/riptide/RiptideDemoApplication.java
@@ -0,0 +1,11 @@
+package com.divinedragon.zalando.riptide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiptideDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RiptideDemoApplication.class, args);
+	}
+}
